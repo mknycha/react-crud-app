20a9d58f4928bc6054b444adc0a0e0c0fa87bd81
Marcin K <marcin.knychala@bitgamelabs.com>
2019-03-22 23:17:17 +0100

    Validate attributes in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,3 @@
 class Post < ApplicationRecord
+  validates :title, :content, :author, presence: true
 end
--- db/migrate/20190319122030_create_posts.rb
+++ db/migrate/20190319122030_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :content
-      t.string :author
+      t.string :title, null: false
+      t.text :content, null: false
+      t.string :author, null: false
 
       t.timestamps
     end
