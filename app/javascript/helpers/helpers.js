c6457161b5382a57ad24ce1fb2ce66644693a946
Marcin K <marcin.knychala@bitgamelabs.com>
2019-04-07 16:10:17 +0200

    Helper file: format code and remove console log

--- app/javascript/helpers/helpers.js
+++ app/javascript/helpers/helpers.js
@@ -1,22 +1,21 @@
-export const validatePost = (post) => {
+export const validatePost = post => {
   const errors = {};
 
-  if (post.title === '') {
-    errors.title = 'You must enter a title';
+  if (post.title === "") {
+    errors.title = "You must enter a title";
   }
 
-  if (post.content === '') {
-    errors.content = 'You must add a content';
+  if (post.content === "") {
+    errors.content = "You must add a content";
   }
 
-  if (post.author === '') {
-    errors.author = 'You must add an author';
+  if (post.author === "") {
+    errors.author = "You must add an author";
   }
 
-  console.log(event);
   return errors;
-}
+};
 
-export const isEmptyObject = (obj) => {
+export const isEmptyObject = obj => {
   return Object.keys(obj).length === 0;
-}
+};
